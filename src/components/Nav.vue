4649da597c0f467ec1dcfcf00d35c5cec63b6dd6
helloaihao <helloaihao@outlook.com>
2017-03-16 19:52:54 +0800

    init

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav v-for="list in lists">
+      <a v-bind:href="list">demo{{list}}</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'nav',
+    data () {
+      return {
+          lists: []
+      }
+    },
+    created () {
+      let num = 8;
+      for(let i = 0; i < num; i ++) {
+        this.lists.push(i);
+      }
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Hello from '@/components/Hello';
+// import Nav from '@/components/Nav';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello,
+    },
+  ],
+});
